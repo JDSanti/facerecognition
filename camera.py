dfb210098d18d8c38cac26847732ec6a8423ef7c
Jose D. Santiago <santiago00005@gmail.com>
2020-08-17 18:58:08 -0400

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Documentation String"""
+#Jose Santiago
+#Apr 14, 2020
+
+#Import Libraries
+import numpy as np
+import cv2
+
+#Opening IP Camera Stream 
+cap = cv2.VideoCapture('http://s7:SamsungGalaxyS7@192.168.1.35:8080/video')
+
+
+while True:
+
+    #Capture Frame-by-Frame
+    ret, frame = cap.read()
+    #Change to gray
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #Display the frame of VideoCapture
+    cv2.imshow("Capturing",frame)
+    #Wait for quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#Release Capture and Destroy Windows when finished
+cap.release()
+cv2.destroyAllWindows()
